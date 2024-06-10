1835a62c61d404d4ab6d7299982b045df95a6b4c
Vlad Kuznetsov <kuzvladik15@mail.ru>
2024-06-11 01:17:17 +0500

    Тестовый коммит

--- src/main/java/com/example/client_server_application/HelloApplication.java
+++ src/main/java/com/example/client_server_application/HelloApplication.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class HelloApplication extends Application {Ы
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Мяу");
         stage.setScene(scene);
         stage.show();
     }
